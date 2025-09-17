5c86d597931a668648ff3207e1922420c201e951
Gustavomullersouza <gustavomullerdesouza@gmail.com>
2025-09-17 20:49:01 -0300

    aaa

--- src/main/kotlin/database/PessoaRepository.kt
+++ src/main/kotlin/database/PessoaRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PessoaRepository : JpaRepository<Pessoa, Long> {aaaa
+interface PessoaRepository : JpaRepository<Pessoa, Long> {
 
 }
