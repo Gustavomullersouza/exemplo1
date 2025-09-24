f82380a16dce94cd7def12d636e59773a5b11d64
Gustavomullersouza <gustavomullerdesouza@gmail.com>
2025-09-24 20:10:29 -0300

    aaa

--- src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
+++ src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
@@ -1,9 +1,9 @@
-package controller
+package br.com.unipar.exemplo.controller
 
-import database.PessoaRepository
-import model.Pessoa
+import br.com.unipar.exemplo.database.PessoaRepository
+import br.com.unipar.exemplo.model.Pessoa
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,5 +22,10 @@ class PessoaController ( private val pessoaRepository : PessoaRepository){
     : ResponseEntity<Pessoa>{
         return ResponseEntity.ok(
             pessoaRepository.save<Pessoa>(pessoa))
+    }
+
+    @GetMapping
+    fun listarPessoa() : List<Pessoa>{
+        return pessoaRepository.findAll()
     }
 }
